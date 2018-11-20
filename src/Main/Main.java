f2a7a7fa1fb4990457f5518c642957326cd8a0f8
Tom HOYO <tom.hoyo@viacesi.fr>
2018-11-20 20:25:40 +0100

    s

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.io.IOException;
+
 import Controller.Controller;
 
 /*
@@ -12,7 +14,7 @@ import Controller.Controller;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		@SuppressWarnings("unused")
 		Controller controller = new Controller();
 
